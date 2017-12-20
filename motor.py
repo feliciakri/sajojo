0e268f8814a9057f6035837d077ed047dfb5b450
Felicia <feliciakrismanta@hotmail.com>
2017-12-20 18:52:01 +0700

    Improved modularity

--- motor.py
+++ motor.py
@@ -4,31 +4,30 @@ import time
 
 
 def rotate(round):
+	seq = [ [1,0,0,0],
+			[1,1,0,0],
+			[0,1,0,0],
+			[0,1,1,0],
+			[0,0,1,0],
+			[0,0,1,1],
+			[0,0,0,1],
+			[1,0,0,1] ]
 	for i in range(round): 
 		for halfstep in range (8):
 			for pin in range (4):
 				GPIO.output(ControlPin[pin], seq[halfstep][pin])
 			time.sleep(0.001)
 
-fullround = 512
 
+# GPIO SETUP
 GPIO.setmode(GPIO.BCM)
-
 ControlPin = [7,11,13,15] #change as needed
-
 for pin in ControlPin:
 	GPIO.setup(pin,GPIO.OUT)
 	GPIO.output(pin,0)
 
-seq = [ [1,0,0,0],
-		[1,1,0,0],
-		[0,1,0,0],
-		[0,1,1,0],
-		[0,0,1,0],
-		[0,0,1,1],
-		[0,0,0,1],
-		[1,0,0,1] ]
-
+# ROTATE EXAMPLE
+fullround = 512 # 512 step di motor = 1 lingkaran penuh
 rotate(fullround) # kalo mau rotate 1 lingkaran penuh
 rotate(fullround/2) # kalo mau rotate 1/2 lingkaran
 
